16a0b810e3093c26ccb55efd0ecdaafabbb9af16
Mustapha <muscoff2008@gmail.com>
2019-05-25 02:44:51 +0000

    Updates

---
+++ precache-manifest.3d24a2baf698a440e1a7b23d4c1970e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec2fa1627ee6f8a697a40fbd0469466",
+    "url": "/talent/index.html"
+  },
+  {
+    "revision": "809077e7f055f8183d7c",
+    "url": "/talent/static/css/main.a6b1e9ed.chunk.css"
+  },
+  {
+    "revision": "c97f30925bc9f2c07bba",
+    "url": "/talent/static/js/2.91930e05.chunk.js"
+  },
+  {
+    "revision": "809077e7f055f8183d7c",
+    "url": "/talent/static/js/main.6ba9499e.chunk.js"
+  },
+  {
+    "revision": "c450cd41c9650c0e9983",
+    "url": "/talent/static/js/runtime~main.fe56a8f5.js"
+  },
+  {
+    "revision": "3ba525d113479de2bfb8222ab451639c",
+    "url": "/talent/static/media/about.3ba525d1.jpg"
+  },
+  {
+    "revision": "4865cc44bf4a30528b95ccab4827d3ae",
+    "url": "/talent/static/media/bg.4865cc44.jpg"
+  },
+  {
+    "revision": "0431bb0740ff4a27204fefd98db99439",
+    "url": "/talent/static/media/community.0431bb07.jpg"
+  },
+  {
+    "revision": "09ff19dc72185ca4f371e89f4f206ab1",
+    "url": "/talent/static/media/line.09ff19dc.png"
+  },
+  {
+    "revision": "4c5f07be47ac0ed765aeba957924c4b9",
+    "url": "/talent/static/media/long.4c5f07be.png"
+  },
+  {
+    "revision": "22e9e53c253eaf4ec07bb80d897585fa",
+    "url": "/talent/static/media/match.22e9e53c.jpg"
+  },
+  {
+    "revision": "d5882f80b11a479637c71cd41b8d3d94",
+    "url": "/talent/static/media/one.d5882f80.jpg"
+  },
+  {
+    "revision": "8a37db67ea402057e6b9845536dd7b4b",
+    "url": "/talent/static/media/patch.8a37db67.jpg"
+  },
+  {
+    "revision": "2aa42e7c2cc811b29ee633a95c507540",
+    "url": "/talent/static/media/three.2aa42e7c.jpg"
+  },
+  {
+    "revision": "622429fd0be88f6b9dc54fb7a2dc84d1",
+    "url": "/talent/static/media/two.622429fd.jpg"
+  }
+]);
